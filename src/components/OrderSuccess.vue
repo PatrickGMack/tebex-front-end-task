05e903b84ea9d6aeac4fdef0f827659259ffa34f
PatrickGMack <patrickgmackintosh@gmail.com>
2025-03-27 19:59:06 +0000

    add fun confetti explosion to checkout

--- src/components/OrderSuccess.vue
+++ src/components/OrderSuccess.vue
@@ -4,8 +4,14 @@
         <div class="flex justify-center items-center border-1 border-[#333333] rounded-[2px] py-5 px-2 md:px-10 lg:px-42 mx-3">
             <div class="text-center md:max-w-[450px]">
                 <div class="flex justify-center">
+                    <!-- Confetti explosion in Tebex colours! -->
+                    <ConfettiExplosion 
+                    :force="0.3"
+                    :colors="['#41c4c3', '#FFFFFF', '#9F9F9F', '#101010']"
+                    />
                     <img src="/img/green_check.png" alt="Checkmark within green circle" class="h-12 w-12 mb-3 pulse"/>
                 </div>
+                
                 <h1 class="text-[26px] font-extrabold text-white mb-4">Order Complete</h1>
                 <p class="text-wrap text-white text-sm">Thank you for your payment. You will recieve your purchased products shortly.</p>
 
@@ -19,6 +25,7 @@
 
 <script lang="ts" setup>
 defineProps<{ order: any }>();
+import ConfettiExplosion from "vue-confetti-explosion";
 </script>
 
 <style scoped>
